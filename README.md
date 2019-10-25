d58068a79bb39bcafa2737bda73183bb26be41a3
lazward <lazwerd@gmail.com>
2019-10-25 17:17:44 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # construct2-SMB3
 WIP recreation of World 1-1 of Super Mario Advance 4: Super Mario Bros. 3 in Construct 2
+
+This is a personal project and is not meant for any sort of profit.
---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1572037963,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/sprite-sheet2.png",
+		"images/tilemap.png",
+		"images/sprite2-sheet0.png",
+		"media/sma4-jump.m4a",
+		"media/sma4-jump.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
